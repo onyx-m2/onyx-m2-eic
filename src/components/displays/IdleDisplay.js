4461c8d9e2dbfb6f865fe902be2721d1b69eb5d0
John McCalla <johnmccalla@users.noreply.github.com>
2021-10-21 22:56:18 -0400

    feat: new idle display

--- src/components/displays/IdleDisplay.js
+++ src/components/displays/IdleDisplay.js
@@ -1,5 +1,10 @@
-import React, { useContext } from 'react'
-import { ThemeContext } from 'styled-components'
+import React from 'react'
+import styled from 'styled-components'
+import { TextUnits } from '../Base'
+import BannerIndicator from '../indicators/BannerIndicator'
+import { ReactComponent as TeslaLogo } from '../../assets/tesla.svg'
+
+const PALE_COLOR = 'rgb(178, 178, 178, 0.5)'
 
 /**
  * Display component that is automatically shown when the car is idle (i.e. the drive
@@ -9,17 +14,16 @@ import { ThemeContext } from 'styled-components'
  * @returns
  */
 export default function IdleDisplay() {
-  const theme = useContext(ThemeContext)
   return (
     <g className='IdleDisplay'>
-      <circle r='38' fill='url(#ball-gradient)' />
-      <text
-        fontFamily={theme.font.family.bold}
-        textAnchor='middle'
-        dominantBaseline='middle'
-      >
-        ONYX
-      </text>
+      <TeslaLogo x='-15' y='-76' width='30' fill={PALE_COLOR} />
+      <PaleText y='20'>PRESS BRAKE PEDAL</PaleText>
+      <PaleText y='26'>TO START CAR</PaleText>
+      <BannerIndicator visible text='ONYX' color='url(#blue-indicator-gradient)' />
     </g>
   )
 }
+
+const PaleText = styled(TextUnits)`
+  fill: ${props => PALE_COLOR};
+`
