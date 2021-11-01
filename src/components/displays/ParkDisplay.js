497029092d0aedcebbd20b7692441df923991fb8
John McCalla <johnmccalla@users.noreply.github.com>
2021-10-31 21:29:31 -0400

    feat: add a battery (remaning) capacity display in park

--- src/components/displays/ParkDisplay.js
+++ src/components/displays/ParkDisplay.js
@@ -1,6 +1,6 @@
 import { useSignalState } from 'onyx-m2-react'
 import React from 'react'
-import { FadeableGroup, HeroTextValue, SecondaryTextValue, TextUnits } from '../Base'
+import { HeroTextValue, SecondaryTextIndicator } from '../Base'
 
 const ODOMETER_SNA = 4294967.295  // SNA is actually 0xffffffff but the dbc calls
                                   // for scaling the value by 0.001, which the code
@@ -14,14 +14,26 @@ const ODOMETER_SNA = 4294967.295  // SNA is actually 0xffffffff but the dbc call
  * @returns
  */
 export default function ParkDisplay() {
-  const odometer = useSignalState('DI_odometer', ODOMETER_SNA)
+  const odometer = useSignalState('UI_odometer', ODOMETER_SNA)
+  const nominalFullPack = useSignalState('BMS_nominalFullPackEnergy', 0)
+  const initialFullPack = useSignalState('BMS_initialFullPackEnergy', 0)
+
+  var odometerText = '-'
+  if (odometer !== ODOMETER_SNA) {
+    odometerText = odometer.toLocaleString(undefined, {maximumFractionDigits: 0})
+  }
+
+  var capacityText = '-'
+  if (nominalFullPack && initialFullPack) {
+    const capacity = nominalFullPack * 100 / initialFullPack
+    capacityText = capacity.toLocaleString(undefined, {maximumFractionDigits: 0}) + '%'
+  }
+
   return (
     <g className='ParkDisplay'>
       <HeroTextValue y={-16}>P</HeroTextValue>
-      <FadeableGroup className='Odometer' visible={odometer !== ODOMETER_SNA}>
-        <SecondaryTextValue y={22}>{odometer.toLocaleString(undefined, {maximumFractionDigits: 0})}</SecondaryTextValue>
-        <TextUnits y={24}>km</TextUnits>
-      </FadeableGroup>
+      <SecondaryTextIndicator x={-18} y={18} value={odometerText} units='km' />
+      <SecondaryTextIndicator x={18}  y={18} value={capacityText} units='capacity' />
     </g>
   )
 }
